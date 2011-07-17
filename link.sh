d897e5c4d9ef5ceaf3bfa99150cbebb42ee7b994
Matthew McCullough <matthewm@ambientideas.com>
2011-07-16 22:45:08 -0600

    Updating link.sh to correctly work on Mac in generating symbolic links in the home dir

--- link.sh
+++ link.sh
@@ -1,33 +1,39 @@
+export SHELLSCONFIGDIR=.shellsconfig
+pushd ~
+
 ###################
 ## Zsh
 ###################
-unlink ~/.zshrc
-unlink ~/.zshenv
-unlink ~/.zsh_history
-unlink ~/.zsh
-ln -s .zshrc ~/.zshrc
-ln -s .zshenv ~/.zshenv
-rm ~/.zsh_history; ln -s .zsh_history ~/.zsh_history
-ln -s .zsh ~/.zsh
+unlink .zshrc
+unlink .zshenv
+unlink .zsh
+unlink .zsh_history
+ln -s $SHELLSCONFIGDIR/.zshrc
+ln -s $SHELLSCONFIGDIR/.zshenv
+ln -s $SHELLSCONFIGDIR/.zsh
+rm .zsh_history; ln -s $SHELLSCONFIGDIR/.zsh_history
 
 ###################
 ## Bash
 ###################
-unlink ~/.bashrc
-unlink ~/.bash_profile
-unlink ~/.bash_history
-unlink ~/.bash_gitprompt
-ln -s .bashrc         ~/.bashrc
-ln -s .bash_profile   ~/.bash_profile
-ln -s .bash_history   ~/.bash_history
-ln -s .bash_gitprompt ~/.bash_gitprompt
+unlink .bashrc
+unlink .bash_profile
+unlink .bash_history
+unlink .bash_gitprompt
+ln -s $SHELLSCONFIGDIR/.bashrc
+ln -s $SHELLSCONFIGDIR/.bash_profile
+ln -s $SHELLSCONFIGDIR/.bash_history
+ln -s $SHELLSCONFIGDIR/.bash_gitprompt
+
 
 ###################
 ## Common
 ###################
-unlink ~/.shellvars
-unlink ~/.shellpaths
-unlink ~/.shellaliases
-ln -s .shellvars      ~/.shellvars
-ln -s .shellpaths     ~/.shellpaths
-ln -s .shellaliases   ~/.shellaliases
+unlink .shellvars
+unlink .shellpaths
+unlink .shellaliases
+ln -s $SHELLSCONFIGDIR/.shellvars
+ln -s $SHELLSCONFIGDIR/.shellpaths
+ln -s $SHELLSCONFIGDIR/.shellaliases
+
+popd
