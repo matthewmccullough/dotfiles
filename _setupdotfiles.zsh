8ef8070872a78be796bf5e269de7c4f7ef2d72b3
Matthew McCullough <matthew@github.com>
2016-01-17 12:55:51 -0800

    Better output in setup script

--- _setupdotfiles.zsh
+++ _setupdotfiles.zsh
@@ -2,24 +2,28 @@
 
 export UNLINK=true
 
+# SymLink the file, if it doesn't already exist
+# 1) Identifies what directory is being worked on.
+# 2) Unlinks, any existing file (if requested by option switch).
+# 3) Create the new link
 function symlinkifne {
     echo "WORKING ON: $1"
-    
+
     # does it exist
     if [[ -a $1 ]]; then
       echo "  WARNING: $1 already exists."
-      
+
       # If Unlink is requested
       if [ "$UNLINK" = "true" ]; then
           unlink $1
           echo "  Unlinking $1"
-          
+
           # create the link
           export DOTLESS=`echo $1 | sed s/.//`
           echo "  Symlinking $DOTFILESDIRRELATIVETOHOME/$DOTLESS to $1"
           ln -s $DOTFILESDIRRELATIVETOHOME/$DOTLESS $1
       else
-        echo "  SKIPPING $1."  
+        echo "  SKIPPING $1."
       fi
     # does not exist
     else
@@ -31,24 +35,25 @@ function symlinkifne {
 }
 
 
-echo "This script must be run from the dotfiles directory"
-echo "Setting up..."
+echo "NOTE: This script must be run from the dotfiles repository directory"
+echo "Determining paths..."
+
+echo "HOME = $HOME"
 
 #export DOTFILESDIRRELATIVETOHOME=$PWD
 export DOTFILESDIRRELATIVETOHOME=.dotfiles
 echo "DOTFILESDIRRELATIVETOHOME = $DOTFILESDIRRELATIVETOHOME"
 
+echo "Creating links..."
 pushd ~
 
 symlinkifne .bash_profile
 symlinkifne .bashrc
-symlinkifne .boom
-symlinkifne .boom.conf
-symlinkifne .cloudapp
+symlinkifne .Brewfile
 symlinkifne .conf
 symlinkifne .gemrc
 symlinkifne .gitconfig
-# symlinkifne .gitignore
+symlinkifne .gitignore
 symlinkifne .netrc
 symlinkifne .profile
 symlinkifne .rvmrc
@@ -60,10 +65,8 @@ symlinkifne .slate
 symlinkifne .vimrc
 symlinkifne .zlogout
 symlinkifne .zprofile
-#symlinkifne .zsh
 symlinkifne .zshenv
 symlinkifne .zshrc
 symlinkifne .vim
 
 popd
-
